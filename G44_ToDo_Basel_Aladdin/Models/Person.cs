2042ca783d67ce4df01df211e2d848de88316630
Alkawarit <91874067+Alkawarit@users.noreply.github.com>
2022-09-19 22:29:04 +0200

    Add project files.

---
+++ G44_ToDo_Basel_Aladdin/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G44_ToDo_Basel_Aladdin.Models
+{
+    internal class Person
+    {
+    }
+}
---
+++ G44_ToDo_Basel_Aladdin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G44_ToDo_Basel_Aladdin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
