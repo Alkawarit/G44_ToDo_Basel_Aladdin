187bdc557c749c8c990e4465a423ccb8262977ac
Alkawarit <91874067+Alkawarit@users.noreply.github.com>
2022-09-23 11:43:05 +0200

    last changes and refactory.

--- G44_ToDo_Basel_Aladdin/Program.cs
+++ G44_ToDo_Basel_Aladdin/Program.cs
@@ -7,16 +7,17 @@ namespace G44_ToDo_Basel_Aladdin
     {
         static void Main(string[] args)
         {
-            ToDoService toDoService = new ToDoService();
+            //    ToDoService toDoService = new ToDoService();
 
-            foreach (ToDo td in toDoService.FindAllTasks())
-            {
-                Console.WriteLine("From the PROGRAM.CS: " + td.Descriptiion);
+            //    foreach (ToDo td in toDoService.FindAllTasks())
+            //    {
+            //        Console.WriteLine("From the PROGRAM.CS: " + td.Descriptiion);
+            //    }
+
+
+
+            //    toDoService.RemoveObjectFromToDoArray(3);
             }
-
-
-
-            toDoService.RemoveObjectFromToDoArray(3);
         }
     }
-    }
+
